87108a98e1ce18fb848e9fdf60d836a40b2dd39c
pacudimfc <kevindi0695@gmail.com>
2021-01-27 17:20:17 -0300

    segundo commit

---
+++ script2.js
@@ -0,0 +1,77 @@
+//Variables y constantes
+
+const botonNumero = document.getElementsByName("botones");
+const dataOpera = document.getElementsByName("operador__hijo");
+const igual = document.getElementById("operador__igual");
+const clear = document.getElementById("clear");
+var resultado = document.getElementById("resultado");
+
+var operacion = "";
+var operandob = "";
+var operandoa = "";
+
+
+//Eventos
+
+botonNumero.forEach((boton) => {
+    boton.addEventListener('click', ()=>{
+        resultado.innerText = resultado.innerText + boton.innerText;
+    
+    })    
+}); 
+
+dataOpera.forEach((boton)=>{
+    boton.addEventListener('click', ()=>{
+
+        operandoa = resultado.innerText; 
+        operacion = boton.innerText;
+        limpiarPantalla();
+
+    })
+    
+});
+
+igual.addEventListener('click', ()=>{
+
+    operandob = resultado.innerText;
+    limpiarPantalla();
+    resolver();
+  
+
+});
+
+clear.addEventListener('click', ()=>{
+
+    limpiarPantalla()
+
+});
+
+//funciones
+
+function limpiarPantalla(){
+    resultado.innerText = "";
+};
+
+function resolver(){
+
+switch (operacion) {
+    case "+" :
+        resultado.innerText = parseFloat(operandoa) + parseFloat(operandob);
+        break;
+    case "-" :
+        resultado.innerText = parseFloat(operandoa) - parseFloat(operandob);
+        break;
+    case "*" :
+        resultado.innerText = parseFloat(operandoa) * parseFloat(operandob);
+        break;
+    case "/" :
+        resultado.innerText = parseFloat(operandoa) / parseFloat(operandob);
+        break;
+
+    default: "Error"
+        break;
+}
+
+
+};
+
